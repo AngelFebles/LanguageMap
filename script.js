ce623c8b3428c83a40667549605ba6f739a99587
Angel Febles <licenciadofebles@gmail.com>
2023-06-14 16:54:50 -0500

    Changed map api from Open Street Map to Map Quest

--- script.js
+++ script.js
@@ -1,110 +1,27 @@
-// Load the GeoJSON file
-fetch('countries.geojson')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    // Call the initMap function and pass the country boundaries data
-    initMap(data);
-  })
-  .catch(function(error) {
-    console.log('Error:', error);
+window.onload = function() {
+  L.mapquest.key = 'ck2OXUAJsF0iz999XGQ62jyXo8AXOVp7';
+
+  var map = L.mapquest.map('map', {
+    center: [39.7392, -104.9903],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 4,
+    minZoom: 3,
+    maxZoom: 8,
+    continuousWorld: true
   });
 
-  function initMap(countriesData) {
-    var passportSelect = document.getElementById('passport');
-    var selectedPassport = passportSelect.value;
+  L.DomUtil.addClass(map._container, 'mapquest-map');
 
-    var southWest = L.latLng(-90, -180); // Southwest coordinates of the map boundary
-    var northEast = L.latLng(90, 180);   // Northeast coordinates of the map boundary
-    var bounds = L.latLngBounds(southWest, northEast);
-    
-    var map = L.map('map', {
-      maxBounds: bounds,  // Set the maximum boundaries
-      maxBoundsViscosity: 1.0, // Adjust the restriction behavior
-      minZoom: 2.6, // Set the minimum zoom level
-      zoomSnap: 0.5 // Adjust the zoom levels to snap at half-intervals
-    }).setView([0, 0], 2);
-    
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-      maxZoom: 18
-    }).addTo(map);
-  
-    var countries = [
-      { name: 'United States', code: 'US', color: '#00FF00' },
-      { name: 'United Kingdom', code: 'UK', color: '#FF0000' },
-      { name: 'Germany', code: 'DE', color: '#0000FF' },
-      // Add more countries with their respective entry requirements
-    ];
-  
-    var geojsonLayer;
-  
-    function styleFeature(feature) {
-      var countryColor = '#CCCCCC';
-      countries.forEach(function(country) {
-        if (country.code === selectedPassport) {
-          if (feature.properties.name === country.name) {
-            countryColor = country.color;
-          }
-        }
-      });
-  
-      return {
-        fillColor: countryColor,
-        weight: 1,
-        opacity: 1,
-        color: 'black',
-        fillOpacity: 0.6
-      };
-    }
-  
-    function highlightFeature(e) {
-      var layer = e.target;
-  
-      layer.setStyle({
-        weight: 2,
-        color: '#666',
-        dashArray: '',
-        fillOpacity: 0.7
-      });
-  
-      if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-        layer.bringToFront();
-      }
-    }
-  
-    function resetHighlight(e) {
-      geojsonLayer.resetStyle(e.target);
-    }
-  
-    function zoomToFeature(e) {
-      map.fitBounds(e.target.getBounds());
-    }
-  
-    function onEachFeature(feature, layer) {
-      layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
-        click: zoomToFeature
-      });
-    }
-  
-    function updateMap() {
-      if (geojsonLayer) {
-        map.removeLayer(geojsonLayer);
-      }
-  
-      selectedPassport = passportSelect.value;
-  
-      geojsonLayer = L.geoJSON(countriesData, {
-        style: styleFeature,
-        onEachFeature: onEachFeature
-      }).addTo(map);
-    }
-  
-    passportSelect.addEventListener('change', updateMap);
-  
-    updateMap();
-  }
-  
+  var maxBounds = L.latLngBounds(L.latLng(-90, -180), L.latLng(90, 180));
+  map.setMaxBounds(maxBounds);
+
+  window.addEventListener('resize', function() {
+    map.invalidateSize();
+  });
+
+  /*
+  $.get('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson', function(data) {
+    L.geoJSON(data).addTo(map);
+  });
+  */
+};
