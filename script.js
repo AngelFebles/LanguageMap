0c5690d83b375b55196549f763d3d8b739f805aa
Angel Febles <licenciadofebles@gmail.com>
2023-06-15 23:44:08 -0500

    Changed map library to amCharts

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
-window.onload = function() {
-  L.mapquest.key = 'ck2OXUAJsF0iz999XGQ62jyXo8AXOVp7';
+am4core.ready(function () {
 
-  var map = L.mapquest.map('map', {
-    center: [39.7392, -104.9903],
-    layers: L.mapquest.tileLayer('map'),
-    zoom: 4,
-    minZoom: 3,
-    maxZoom: 8,
-    continuousWorld: true
-  });
+  // Themes begin
+  am4core.useTheme(am4themes_animated);
+  // Themes end
 
-  L.DomUtil.addClass(map._container, 'mapquest-map');
+  // Create map instance
+  var chart = am4core.create("chartdiv", am4maps.MapChart);
 
-  var maxBounds = L.latLngBounds(L.latLng(-90, -180), L.latLng(90, 180));
-  map.setMaxBounds(maxBounds);
+  // Set map definition
+  chart.geodata = am4geodata_worldLow;
 
-  window.addEventListener('resize', function() {
-    map.invalidateSize();
-  });
+  // Set projection
+  chart.projection = new am4maps.projections.NaturalEarth1();
 
-  /*
-  $.get('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson', function(data) {
-    L.geoJSON(data).addTo(map);
-  });
-  */
-};
+  // Create map polygon series
+  var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+  polygonSeries.mapPolygons.template.strokeWidth = 0.5;
+
+  // Exclude Antartica
+  polygonSeries.exclude = ["AQ"];
+
+  // Make map load polygon (like country names) data from GeoJSON
+  polygonSeries.useGeodata = true;
+
+  // Configure series
+  var polygonTemplate = polygonSeries.mapPolygons.template;
+  polygonTemplate.tooltipText = "{name}";
+  polygonTemplate.fill = chart.colors.getIndex(0);
+
+  // Create hover state and set alternative fill color
+  var hs = polygonTemplate.states.create("hover");
+  hs.properties.fill = chart.colors.getIndex(2);
+
+  // Create active state
+  var activeState = polygonTemplate.states.create("active");
+  activeState.properties.fill = chart.colors.getIndex(4);
+
+  // Create an event to toggle "active" state
+  polygonTemplate.events.on("hit", function (ev) {
+    ev.target.isActive = !ev.target.isActive;
+  })
+
+
+  var graticuleSeries = chart.series.push(new am4maps.GraticuleSeries());
+
+});
