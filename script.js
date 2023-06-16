271c13ce5a70ecf7a49af69bb3e0976b6238cb7f
Angel Febles <licenciadofebles@gmail.com>
2023-06-16 04:34:42 -0500

    Added basic color changing for each language

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-am4core.ready(function () {
-
+am4core.ready(function() {
   // Themes begin
   am4core.useTheme(am4themes_animated);
   // Themes end
@@ -17,7 +16,7 @@ am4core.ready(function () {
   var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
   polygonSeries.mapPolygons.template.strokeWidth = 0.5;
 
-  // Exclude Antartica
+  // Exclude Antarctica
   polygonSeries.exclude = ["AQ"];
 
   // Make map load polygon (like country names) data from GeoJSON
@@ -32,11 +31,82 @@ am4core.ready(function () {
   var hs = polygonTemplate.states.create("hover");
   hs.properties.fill = chart.colors.getIndex(2);
 
- // Set default zoom level
- chart.homeZoomLevel = 0; // Adjust the value as per your preference
+  // Set up the country-specific language color
+  var languageColor = chart.colors.getIndex(3);
 
- 
+  // Add an additional state to preserve language color when not hovered
+  var fixedState = polygonTemplate.states.create("fixed");
+  fixedState.properties.fill = languageColor;
 
-  var graticuleSeries = chart.series.push(new am4maps.GraticuleSeries());
+  // Language data
+  var languageData;
 
+  // Fetch language data from JSON file
+  function fetchLanguageData() {
+    fetch("languages_codes.json")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        languageData = data;
+      })
+      .catch(function(error) {
+        console.log("Error fetching language data:", error);
+      });
+  }
+
+  // Change country colors based on selected language
+  function changeLanguageColor(language) {
+    // Reset colors for all countries
+    polygonSeries.mapPolygons.each(function(polygon) {
+      polygon.fill = chart.colors.getIndex(0);
+    });
+
+    // Find the corresponding countries for the selected language
+    var countries = languageData.countries.find(function(item) {
+      return item.language === language;
+    });
+
+    if (countries) {
+      // Change the color of the countries for the selected language
+      polygonSeries.mapPolygons.each(function(polygon) {
+        if (countries.countries.includes(polygon.dataItem.dataContext.id)) {
+          polygon.fill = languageColor;
+        }
+      });
+    }
+  }
+
+  // Initialize the map and language data
+  function initialize() {
+    fetchLanguageData();
+  }
+
+  // Event listeners for language buttons
+  document.getElementById("es").addEventListener("click", function() {
+    changeLanguageColor("es");
+  });
+
+  document.getElementById("en").addEventListener("click", function() {
+    changeLanguageColor("en");
+  });
+
+  document.getElementById("fr").addEventListener("click", function() {
+    changeLanguageColor("fr");
+  });
+
+  document.getElementById("ru").addEventListener("click", function() {
+    changeLanguageColor("ru");
+  });
+
+  document.getElementById("zh").addEventListener("click", function() {
+    changeLanguageColor("zh");
+  });
+
+  document.getElementById("ar").addEventListener("click", function() {
+    changeLanguageColor("ar");
+  });
+
+  // Call the initialize function to start the map and fetch language data
+  initialize();
 });
